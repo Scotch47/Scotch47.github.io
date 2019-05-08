a63305289e6b8b503f39b4cf75fa6124e9dff165
Vitaly <Scotch47@mail.ru>
2019-05-08 18:09:59 +0300

    js upd

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-var modal = document.getElementById('myModal');
-
+var body = document.querySelector("body");
+  var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
-
 var span = document.getElementsByClassName("close")[0];
-
 btn.onclick = function() {
     modal.style.display = "block";
+    body.style.overflow = "hidden";
+
 }
 
 span.onclick = function() {
     modal.style.display = "none";
+    body.style.overflow = "visible";
+
 }
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    body.style.overflow = "visible";
+
     }
 }
