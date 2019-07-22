d5b54d3ff0672545a05c3eca872f745f6e0d94ae
Vitaly <Scotch47@mail.ru>
2019-07-22 21:12:44 +0300

    add new work

---
+++ Lenoma/js/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+  $('.slide-wrapper').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+
+  dots: true,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  accessibility: true,
+  speed: 1200,
+  });
+
+
+  $('.slider-blocks').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  adaptiveHeight: true,
+  infinite: false,
+  arrows: true,
+  prevArrow: '<button class="prev arrows"></button>',
+  nextArrow: '<button class="next arrows"></button>',
+  accessibility: true,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 580,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+
+});
+
+    $('.slider-brend').slick({
+      slidesToShow: 5,
+      slidesToScroll: 2,
+    adaptiveHeight: true,
+    arrows: true,
+    infinite:false,
+    prevArrow: '<button class="prev arrows"></button>',
+    nextArrow: '<button class="next arrows"></button>',
+    accessibility: true,
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+
+
+
+
+
+
+
+
+});
+
+	// menu
+  (function ($) {
+    $(function () {
+      $('.menu__icon').on('click', function () {
+        $(this).closest('.menu')
+          .toggleClass('menu_state_open');
+      });
+
+      $('.menu__links-item').on('click', function () {
+       
+
+        $(this).closest('.menu')
+          .removeClass('menu_state_open');
+      });
+    });
+  })(jQuery);
+
+  
+
+});
