643a40d73160a09c35124a17ac8b65abf9205d75
Vitaly <Scotch47@mail.ru>
2019-07-27 18:13:11 +0300

    Rebuild adaptive menu

--- Lenoma/js/main.js
+++ Lenoma/js/main.js
@@ -97,22 +97,13 @@ $(document).ready(function(){
 
 });
 
-	// menu
-  (function ($) {
-    $(function () {
-      $('.menu__icon').on('click', function () {
-        $(this).closest('.menu')
-          .toggleClass('menu_state_open');
-      });
+	
 
-      $('.menu__links-item').on('click', function () {
-       
+  $('#main-nav').hcOffcanvasNav({
+    maxWidth: 991
+  });
 
-        $(this).closest('.menu')
-          .removeClass('menu_state_open');
-      });
-    });
-  })(jQuery);
+
 
   
 
